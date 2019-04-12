ffb0a7575fb1aa6e20502de63051353418fc9ce2
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-12 14:24:36 -0700

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +83,11 @@ def adding_matz
         }
      }
 
-    
+     programmer_hash[:yukihiro_matsumoto] = {
+       known_for: "Ruby",
+       languages: ["LISP", "C"]
+     }
+    programmer_hash
 end
 
 def changing_alan
@@ -101,8 +108,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -122,4 +130,7 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+
+     programmer_hash
 end
